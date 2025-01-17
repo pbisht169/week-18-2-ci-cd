7d8590be3f08ac932086131fc782095c0ebe435b
pbisht17 <pawanbisht94.pb@gmail.com>
2025-01-17 08:38:01 +0530

    fix

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -10,6 +10,7 @@ app.use(express.json())
 
 app.post("/hdfcWebhook", async (req, res) => {
     //TODO: Add zod validation here?
+    //TODO: hello Javascript?
     //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
     const paymentInformation: {
         token: string;
